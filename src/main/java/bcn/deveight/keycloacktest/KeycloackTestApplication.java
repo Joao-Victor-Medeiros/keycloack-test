6683fd65f06fab33bace42f68d367543cd8a4ae7
bcndeveight <joaovictormedeiros1505@gmail.com>
2026-09-11 15:31:16 -0300

    first commit

---
+++ src/main/java/bcn/deveight/keycloacktest/KeycloackTestApplication.java
@@ -0,0 +1,13 @@
+package bcn.deveight.keycloacktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bcn/deveight/keycloacktest/KeycloackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package bcn.deveight.keycloacktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
