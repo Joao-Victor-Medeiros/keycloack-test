17be8933c5d0dcb435408d9128945b2ebb6f0554
bcndeveight <joaovictormedeiros1505@gmail.com>
2026-09-17 15:34:24 -0300

    feat: first version of modeling paciente user

---
+++ src/main/java/bcn/deveight/keycloacktest/user/User.java
@@ -0,0 +1,21 @@
+package bcn.deveight.keycloacktest.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+    private String perfil;
+    private String endereco;
+    private Date dataNascimento;
+    private Double renda;
+    private List<String> interesses;
+    private List<String> condicaoAcesso;
+    private String historicoSaude;
+    private List<String> solicitacoesAtendimento;
+}
